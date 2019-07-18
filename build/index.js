a6c8d1d8e16877b54d3c2c59fbc17cdd31fa6531
jackjennings <jack@standard-library.com>
2019-07-18 16:47:36 -0700

    Update babel dependency for security issues

--- build/index.js
+++ build/index.js
@@ -3,28 +3,35 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports["default"] = void 0;
 
 var _kefir = require("kefir");
 
 function createElementWithClass(kind, className) {
   var element = document.createElement(kind);
-
   element.className = className;
-
   return element;
 }
 
 function clicker(kind) {
   var element = createElementWithClass("div", kind);
+
   var click = _kefir.Kefir.fromEvents(element, "click");
+
   var mousedown = _kefir.Kefir.fromEvents(element, "mousedown");
+
   var mouseup = _kefir.Kefir.fromEvents(element, "mouseup");
 
   click.onValue(function (e) {
     return e.preventDefault();
   });
-
-  return { element: element, click: click, mousedown: mousedown, mouseup: mouseup };
+  return {
+    element: element,
+    click: click,
+    mousedown: mousedown,
+    mouseup: mouseup
+  };
 }
 
-exports.default = clicker;
+var _default = clicker;
+exports["default"] = _default;
